b4b564879c4faa8d82ccf25edd2a1c136147f2e0
Yakumo Ran <31311154+ran-sama@users.noreply.github.com>
2025-01-06 14:29:05 +0100

    Update cleanup.sh

--- cleanup.sh
+++ cleanup.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env sh
 echo $CERTBOT_DOMAIN
 echo $CERTBOT_VALIDATION
-ssh api@dynv6.com hosts $CERTBOT_DOMAIN records del _acme-challenge txt data $CERTBOT_VALIDATION
-sleep 9
+ssh -i /home/ran/.ssh/dynv6 api@dynv6.com hosts $CERTBOT_DOMAIN records del _acme-challenge txt
+sleep 10
 exit 0
